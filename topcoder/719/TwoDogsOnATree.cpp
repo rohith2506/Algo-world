b88cb5aec527a8e28b4d0525ca66a01389ffb065
Rohith Uppala <rohith.uppala369@gmail.com>
2017-10-02 09:34:21 +0200

    SRM 719

---
+++ topcoder/719/TwoDogsOnATree.cpp
@@ -0,0 +1,148 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+
+class TwoDogsOnATree {
+    public:
+    int maximalXorSum(vector<int> parent, vector<int> w) {
+        int x[1020];
+        for(int i=1; i<parent.size()+1; i++) {
+            x[i] = x[parent[i-1]] ^ w[i-1];
+        }
+        set<int> st;
+        for(int i=0; i<parent.size()+1; i++) {
+            for(int j=0; j<i; j++) {
+                st.insert(x[i] ^ x[j]);
+            }
+        }
+        set<int>::iterator it1;
+        set<int>::iterator it2;
+        int result = 0;
+        for(it1 = st.begin(); it1 != st.end(); it1++) {
+            for(it2 = st.begin(); it2 != st.end(); it2++) {
+                result = max(result, *it1 ^ *it2);
+            }
+        }
+        return result;
+    }
+};
+
+// CUT begin
+ifstream data("TwoDogsOnATree.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> parent, vector<int> w, int __expected) {
+    time_t startClock = clock();
+    TwoDogsOnATree *instance = new TwoDogsOnATree();
+    int __result = instance->maximalXorSum(parent, w);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> parent;
+        from_stream(parent);
+        vector<int> w;
+        from_stream(w);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(parent, w, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1506926753;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 1000 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "TwoDogsOnATree (1000 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
